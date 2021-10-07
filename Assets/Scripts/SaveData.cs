e62a6588d161c1de6a914a15e44e04a29e493182
prof_yaffle <msparshatt@gmail.com>
2021-10-07 22:16:40 +0100

    Moved save data struct to seperate file

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,13 @@
+public struct SaveData
+{
+    public int version;
+    public int terrainResolution;
+    public byte[] heightmap;
+    public int baseTexture;
+
+    public byte[] baseTexture_colors;
+    public float tiling;
+    public bool aoActive;
+    public byte[] overlayTexture;
+    public float paintTiling;
+} 
