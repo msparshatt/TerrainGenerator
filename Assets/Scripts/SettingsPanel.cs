11893bd0bd9a5eb51c4eef68c60511442597f740
prof_yaffle <msparshatt@gmail.com>
2022-01-11 16:07:21 +0000

    Save the screenshot resolution value and clear with reset button

--- Assets/Scripts/SettingsPanel.cs
+++ Assets/Scripts/SettingsPanel.cs
@@ -8,6 +8,7 @@ public class SettingsPanel : MonoBehaviour
     [SerializeField] private Slider movementSlider;
     [SerializeField] private Slider sensitivitySlider;
     [SerializeField] private SettingsDataScriptable dataScriptable;
+    [SerializeField] private Dropdown ResolutionDropdown;
 
     private float defaultSpeed = 40.0f;
     private float defaultSensitivity = 1.0f;
@@ -25,6 +26,7 @@ public class SettingsPanel : MonoBehaviour
     {
         movementSlider.value = defaultSpeed;
         sensitivitySlider.value = defaultSensitivity;
+        ResolutionDropdown.value = 0;
     }
 
     public void ReloadButtonClick()
@@ -35,6 +37,9 @@ public class SettingsPanel : MonoBehaviour
 
         if(dataScriptable.defaultTerrainResolution == 0)
             dataScriptable.defaultTerrainResolution = 1025;
+
+        ResolutionDropdown.value = PlayerPrefs.GetInt("ScreenshotResolution");
+        //ResolutionDropDownChange(PlayerPrefs.GetInt("ScreenshotResolution"));
     }
 
     public void ResolutionDropDownChange(int index)
@@ -47,6 +52,7 @@ public class SettingsPanel : MonoBehaviour
         PlayerPrefs.SetFloat("movementSpeed", movementSlider.value);
         PlayerPrefs.SetFloat("cameraSensitivity", sensitivitySlider.value);
         PlayerPrefs.SetInt("DefaultTerrainResolution", dataScriptable.defaultTerrainResolution);
+        PlayerPrefs.SetInt("ScreenshotResolution", ResolutionDropdown.value);
     }
     public void Start()
     {
