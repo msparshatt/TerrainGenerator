6992d03995e6485c738f909426725418bb7ec0b2
prof_yaffle <msparshatt@gmail.com>
2022-04-16 11:25:27 +0100

    Changed default heightmap resolution to 513

--- Assets/Scripts/DataObjects/SettingsDataScriptable.cs
+++ Assets/Scripts/DataObjects/SettingsDataScriptable.cs
@@ -7,7 +7,7 @@ public class SettingsDataScriptable : ScriptableObject
 {
     public float movementSpeed = 40.0f;
     public float cameraSensitivity = 1.0f;
-    public int defaultTerrainResolution = 1025;
+    public int defaultTerrainResolution = 513;
     public int resolutionMultiplier = 1;
     public Color selectedColor;
     public Color deselectedColor;
