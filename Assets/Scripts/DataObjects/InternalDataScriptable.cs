825becd02d6cf351a0a2a3367fa685b77f166f49
prof_yaffle <msparshatt@gmail.com>
2022-04-21 13:44:21 +0100

    Add sky and water settings to the internal data object

--- Assets/Scripts/DataObjects/InternalDataScriptable.cs
+++ Assets/Scripts/DataObjects/InternalDataScriptable.cs
@@ -13,18 +13,57 @@ public class InternalDataScriptable : ScriptableObject
     public bool unsavedChanges = false; //Record if there have been any changes
     public bool detectMaximaAndMinima = false; //Do we need to check for maxima and minima
 
-    public bool ambientOcclusion;
     public Modes mode;
 
+    //imported data
     public List<string> customMaterials;
     public List<string> customPaintBrushes;
     public List<string> customSculptBrushes;
     public List<string> customStampBrushes;
     public List<string> customTextures;
 
+
+
+    //material panel
+    public bool ambientOcclusion;
+    public int[] currentMaterialIndices;
     public float materialScale;
+
+    public int[] mixTypes;
+    public float[] mixFactors;
+
+    //sculpt panel
+
+    //stamp panel
+
+    //paint panel
     public float paintScale;
 
-    public int[] currentMaterialIndices;
+    //sky panel
+    public bool lightTerrain;
+    public float sunHeight;
+    public float sunDirection;
+    public bool automaticColor;
+    public Color sunColor;
+    public bool cloudActive;
+    public float cloudXoffset;
+    public float cloudYOffset;
+    public float cloudScale;
+    public float cloudStart;
+    public float cloudEnd;
+    public float windDirection;
+    public float windSpeed;
+    public float cloudIterations;
 
+
+    //water panel
+    public bool oceanActive;
+    public float oceanHeight;
+    public float waveDirection;
+    public float waveSpeed;
+    public float waveHeight;
+    public float waveChoppyness;
+    public float foamAmount;
+    public bool shoreLineActive;
+    public float shorelineFoamAmount;
 }
