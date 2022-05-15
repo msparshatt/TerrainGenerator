97c91ef9e6d1bfd54f89a4df2f8aa704c60aba01
prof_yaffle <msparshatt@gmail.com>
2022-05-15 12:07:26 +0100

    Added struct to hold save data for the water panel

---
+++ Assets/Scripts/SaveData/WaterSaveData_v1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct WaterSaveData
+{
+    public bool oceanActive;
+    public float oceanHeight;
+    public float waveDirection;
+    public float waveSpeed;
+    public float waveHeight;
+    public float waveChoppyness;
+    public float foamAmount;
+    public bool shoreLineActive;
+    public float shorelineFoamAmount;
+}
