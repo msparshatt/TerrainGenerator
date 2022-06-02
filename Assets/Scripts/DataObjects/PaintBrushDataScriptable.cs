aff6bb84caefb65b3f2991906e875a1a29615a1e
prof_yaffle <msparshatt@gmail.com>
2022-06-02 23:15:52 +0100

    Removed duplicate getMask function

--- Assets/Scripts/DataObjects/PaintBrushDataScriptable.cs
+++ Assets/Scripts/DataObjects/PaintBrushDataScriptable.cs
@@ -18,20 +18,4 @@ public class PaintBrushDataScriptable : BrushDataScriptable
     public MixTypes filterType;
     public float filterFactor;
     public Texture2D paintMask;
-
-    public float[,] getMask(int length, int width)
-    {
-        float scaleX = brush.width / (width * 1.0f);
-        float scaleY = brush.height / (length * 1.0f);
-        float[,] mask = new float[length, width];
-
-        for(int x = 0; x < width; x++) {
-            for(int y = 0; y < length; y++) {
-                //Debug.Log(brush.GetPixel((int)(x * scaleX), (int)(y * scaleY)).r);
-                mask[y,x] = brush.GetPixel((int)(x * scaleX), (int)(y * scaleY)).r;
-            }
-        }
-
-        return mask;
-    }
 }
