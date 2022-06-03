11cf92721f4b3286c09d6344bf985050803de2f5
prof_yaffle <msparshatt@gmail.com>
2022-06-03 16:50:50 +0100

    Class for saving post processing settings

---
+++ Assets/Scripts/SaveData/PostProcessingSaveData_v1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessingSaveData_v1
+{
+    public float chromaticAberrationStrengh;
+    public float vignetteStrength;
+    public Color vignetteColor;
+    public float exposure;
+    public float contrast;
+    public float saturation;
+    public float temperature;
+    public float tint;
+    public float hueShift;
+
+}
