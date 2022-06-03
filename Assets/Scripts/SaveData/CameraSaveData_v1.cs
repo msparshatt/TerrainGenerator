cd419d850096f0cc720fbbc6c3e6ebd1a4a657a7
prof_yaffle <msparshatt@gmail.com>
2022-06-03 22:32:32 +0100

    Class for saving any data regarding the cameras

---
+++ Assets/Scripts/SaveData/CameraSaveData_v1.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSaveData_v1 
+{
+    public List<string> cameraPositions;
+    public int cameraIndex;
+}
